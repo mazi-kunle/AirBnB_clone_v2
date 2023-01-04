fd9a07ae7b08d5cf3db8af91ddce53109892eb87
olakunle <olakunlemichael644@gmail.com>
2023-01-03 19:08:22 -0800

    edit

--- models/city.py
+++ models/city.py
@@ -2,6 +2,7 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import String, Column, ForeignKey
+from sqlalchemy.orm import relationship, backref
 from models.place import Place
 
 
