ec87cd21c962387b2e90b8e164a29661a6620910
olakunle <olakunlemichael644@gmail.com>
2023-01-03 19:10:53 -0800

    edit

--- models/place.py
+++ models/place.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import String, Column, Integer, Float
+from sqlalchemy import String, Column, Integer, Float, ForeignKey
 
 
 class Place(BaseModel, Base):
