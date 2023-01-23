104591ee78afa03fcbbbabc5cb632c227150cfd4
mazi-kunle <olakunlemichael644@gmail.com>
2023-01-23 15:43:19 -0800

    task 0

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# configure a web server for thr deployment of web_static
+sudo apt-get install nginx
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo chown -R "$USER":"$USER" /data/
+content=\
+"<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>"
+echo "$content" > /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo sed -i '29 a\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
+sudo service nginx restart
