7705a55a9f6ae4585c0f64f8dd6d8badb9c68feb
mazi-kunle <olakunlemichael644@gmail.com>
2023-01-24 15:05:13 -0800

    0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,16 +2,15 @@
 # configure a web server for thr deployment of web_static
 sudo apt-get -y update
 sudo apt-get -y install nginx
+
 sudo mkdir -p /data/
-sudo chmod 755 /data/
+
 sudo mkdir -p /data/web_static/releases/
-sudo chmod 777 /data/web_static/
-sudo chmod 755 /data/web_static/releases
+
 sudo mkdir -p /data/web_static/shared/
-sudo chmod 755 /data/web_static/shared/
+
 sudo mkdir -p /data/web_static/releases/test/
-sudo chmod 777 /data/web_static/releases/test/
-sudo chown -R ubuntu:ubuntu /data/
+
 echo "<html>
   <head>
   </head>
@@ -21,4 +20,7 @@ echo "<html>
 </html>" > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo sed -i '29 a\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
+
+sudo chown -R ubuntu:ubuntu /data/
+
 sudo service nginx restart
