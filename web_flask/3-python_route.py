7f99ccb149211c81c8f1bc1467a86f559029ece7
mazi-kunle <102045752+mazi-kunle@users.noreply.github.com>
2023-02-06 23:13:26 +0100

    Update 3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -32,13 +32,13 @@ def cisfun(text):
     return 'C {}'.format(escape(text.replace('_', ' ')))
 
 
-@app.route('/python/', defaults={'text': 'cool'})
+@app.route('/python/', defaults={'text': 'is_cool'})
 @app.route('/python/<text>', strict_slashes=False)
 def python(text):
     '''
     display python <text>
     '''
-    return 'Pythom {}'.format(escape(text.replace('_', ' ')))
+    return 'Python {}'.format(escape(text.replace('_', ' ')))
 
 
 if __name__ == '__main__':
