b67665d78379222252fb99a5f10f2e198d8d6841
mazi-kunle <olakunlemichael644@gmail.com>
2023-02-06 14:10:51 -0800

    3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+'''This is a module'''
+
+from flask import Flask
+from markupsafe import escape
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    '''
+    display hello HBNB
+    '''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display():
+    '''
+    display hbnb
+    '''
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    '''
+    display c <text>
+    '''
+    return 'C {}'.format(escape(text.replace('_', ' ')))
+
+
+@app.route('/python/', defaults={'text': 'cool'})
+@app.route('/python/<text>', strict_slashes=False)
+def python(text):
+    '''
+    display python <text>
+    '''
+    return 'Pythom {}'.format(escape(text.replace('_', ' ')))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
