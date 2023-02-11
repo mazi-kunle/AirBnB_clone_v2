b231ca0e266519c6a6dd21bd30abf13bcaf60854
mazi-kunle <102045752+mazi-kunle@users.noreply.github.com>
2023-02-11 16:24:02 +0100

    Update place.py

--- models/place.py
+++ models/place.py
@@ -23,8 +23,8 @@ place_amenity = Table('place_amenity', Base.metadata,
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = 'places'
-    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    city_id = Column(String(60, collation='utf8mb4_0900_ai_ci'), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60, collation='utf8mb4_0900_ai_ci'), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
     description = Column(String(1024), nullable=True)
     number_rooms = Column(Integer, nullable=False, default=0)
