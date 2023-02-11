c0b2b270f5118773f984ca92aca3c2b555758116
mazi-kunle <102045752+mazi-kunle@users.noreply.github.com>
2023-02-11 17:34:07 +0100

    Update place.py

--- models/place.py
+++ models/place.py
@@ -10,11 +10,11 @@ import os
 
 place_amenity = Table('place_amenity', Base.metadata,
                       Column('place_id',
-                             String(60),
+                             String(60, collation='latin1_swedish_ci'),
                              ForeignKey('places.id'),
                              primary_key=True, nullable=False),
                       Column('amenity_id',
-                             String(60),
+                             String(60, collation='latin1_swedish_ci'),
                              ForeignKey('amenities.id'),
                              primary_key=True, nullable=False)
                       )
