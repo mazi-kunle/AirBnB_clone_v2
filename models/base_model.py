11911af12904c03d4283c9a18299dbd0a05f578f
mazi-kunle <102045752+mazi-kunle@users.noreply.github.com>
2023-02-11 11:11:23 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -10,7 +10,7 @@ Base = declarative_base()
 
 class BaseModel:
     """A base class for all hbnb models"""
-    id = Column(String(60), primary_key=True, unique=True, nullable=False)
+    id = Column(String(60, collation='utf8mb4_0900_ai_ci'), primary_key=True, unique=True, nullable=False)
     created_at = Column(DateTime(), nullable=False, default=datetime.utcnow())
     updated_at = Column(DateTime(), nullable=False, default=datetime.utcnow())
 
