01306b917e6ee8244ec0052153990a11eb6375bd
mazi-kunle <olakunlemichael644@gmail.com>
2023-02-12 06:49:36 -0800

    1

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+'''This is a module'''
+
+from flask import Flask
+from flask import render_template
+from models import storage
+from models.state import State
+from models.amenity import Amenity
+
+
+app = Flask(__name__)
+
+
+@app.route('/hbnb_filters', strict_slashes=False)
+def hbnb():
+    '''
+    handle /hbnb_filters route
+    '''
+    states = storage.all(State)
+    amenities = storage.all(Amenity)
+    return render_template('10-hbnb_filters.html',
+                           states=states, amenities=amenities)
+
+
+@app.teardown_appcontext
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
